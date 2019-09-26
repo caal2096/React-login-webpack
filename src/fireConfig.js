4ea619c36d636213c08f0499a8779148b400d0d5
Cesararmas96 <caal2096@gmail.com>
2019-09-25 21:42:31 -0400

    fireConfig.js

---
+++ src/fireConfig.js
@@ -0,0 +1,9 @@
+export default {
+	apiKey: "AIzaSyCkqULGjxAkfcTL1J_6h0c77Tt0uzmkF7U",
+	authDomain: "webpack-react-login.firebaseapp.com",
+	databaseURL: "https://webpack-react-login.firebaseio.com",
+	projectId: "webpack-react-login",
+	storageBucket: "",
+	messagingSenderId: "377496298688",
+	appId: "1:377496298688:web:005a1efc23233ec93f3925"
+};
